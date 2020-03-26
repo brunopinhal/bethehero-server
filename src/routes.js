cf1297e4a68b42538dc4e6962246100b12d9f8d5
Bruno Pinhal <b.seek2@gmail.com>
2020-03-26 00:13:24 -0300

    Query for list 5 incidents per page

--- src/routes.js
+++ src/routes.js
@@ -19,5 +19,4 @@ routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
-
 module.exports = routes;

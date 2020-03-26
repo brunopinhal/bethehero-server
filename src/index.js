5dfbdc6f5e7e8ef1090431279303c35bb8fc4f23
Bruno Pinhal <b.seek2@gmail.com>
2020-03-26 00:28:02 -0300

    Incidents return infos from Ongs_table into the object

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // avisa o express que estaremos usando json
 app.use(routes);
 
